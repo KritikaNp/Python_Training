f19456c1d434cbe784a9fe2a1e75d37ddc1311ec
kritika neupane <kneupane057@gmail.com>
2026-05-05 19:06:52 +0545

    Adding one more route

--- python_practice/Day10/projects.py
+++ python_practice/Day10/projects.py
@@ -6,13 +6,18 @@ app=Flask(__name__)
 def home():
     name="Kritika"
     age=20
-    projects=["Weather App","Calculator","Shopping Cart"]
+    projects=["Weather App","Calculator","Shopping Cart","Chatbot"]
     return render_template ('index1.html',name=name,age=age,projects=projects)
 
 @app.route('/greet', methods=['POST'])
 def greet():
     name = request.form['username']
     return f"Hello {name}!"
+
+@app.route('/contact')
+def contact():
+    return "Contact me at: kritikaneupane58@email.com"
+
 
 if __name__=='__main__':
     app.run(debug=True)
